220a658913cae10b26f4d21b76de8a130ee9e136
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-09-27 16:13:39 +0200

    opdr 1 debugged

--- debug-me-1.js
+++ debug-me-1.js
@@ -3,12 +3,12 @@ const weight = 82;
 
 const BMI = weight / (height * height);
 
-console.log("Your BMI is: " + BMI.tofixed());
+console.log("Your BMI is: " + BMI.toFixed());
 
-if (BMI > 25 {
+if (BMI > 25 ) {
   console.log("Depending on your build, you might be overweight");
 } else if (BMI < 18) {
-  console.log("Depending on your build, you might be underweight);
+  console.log("Depending on your build, you might be underweight");
 } else {
   console.log("Looks like your height / weight ratio is pretty healthy");
 }
